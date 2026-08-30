9a76ddded1ba861e35b9c6d168e2ee45b7efa271
Vaibhav Khandelwal <khandelwalvaibhav2006@gmail.com>
2026-08-30 23:51:10 +0530

    adding data

--- agents/sql_analyst.py
+++ agents/sql_analyst.py
@@ -15,4 +15,19 @@ from Models.schema import AgentSchema
 
 def curate_question(state: AgentSchema) -> AgentSchema:
 
+    user_question = state.user_question 
+    llm = pick_llm("low")
+    response = llm.invoke(f"Curate the following question : {user_question}")
+
+    state.curated_ques = response
+    return state.curated_ques
+
+
+def prompt_query_context(state: AgentSchema) -> AgentSchema:
+
+    curated_question = state.curated_ques
+
+    
+
+
     
