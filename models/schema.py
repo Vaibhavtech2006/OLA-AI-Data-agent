f0f1570b7a11d0258da7da8aa2f6b7f485e74ed3
Vaibhav Khandelwal <khandelwalvaibhav2006@gmail.com>
2026-09-03 02:27:14 +0530

    adding etl tools

--- models/schema.py
+++ models/schema.py
@@ -17,5 +17,6 @@ class JudgeSchema(BaseModel):
     answer : Literal["Yes","No"] = Field(..., description="Indicates whether the generated SQL query is safe to execute or not")
     comments : str = Field(..., description="Additional comments or feedback from the judge regarding the SQL query")
 
-    
+    class ETLAgentSchema(BaseModel):
+    messages : Annotated[list,add] = Field(..., description="List of messages to be processed by the ETL agent")
 
